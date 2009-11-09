970fb93621e58816f03c2b637a3530758e03ba04
Raphael Simon <raphael@rightscale.com>
2009-11-09 10:17:19 -0800

    Patch ports.conf

--- cookbooks/app_homepage/recipes/update.rb
+++ cookbooks/app_homepage/recipes/update.rb
@@ -3,3 +3,9 @@ template '/mnt/www/index.html' do
   variables( :page_content => @node[:app_homepage][:page_content],
              :page_title   => @node[:app_homepage][:page_title] )
 end
+
+ruby 'fix ports.conf' do
+  ports_path = '/etc/apache2/ports.conf'
+  content = IO.read(ports_path).gsub('8000', '80')
+  File.open('ports_path', 'w') { |f| f.puts content }
+end
