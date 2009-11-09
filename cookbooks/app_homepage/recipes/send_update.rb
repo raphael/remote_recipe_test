b9b23f2b9343ea762e712026af51f93e4285963c
Raphael Simon <raphael@rightscale.com>
2009-11-09 10:51:50 -0800

    Fix recipe name

--- cookbooks/app_homepage/recipes/send_update.rb
+++ cookbooks/app_homepage/recipes/send_update.rb
@@ -1,5 +1,5 @@
 remote_recipe 'send_update' do
-  recipe 'update'
+  recipe 'app_homepage::update'
   recipients_tags @node[:app_homepage][:update][:target]
   attributes :app_homepage => { :page_title   => @node[:app_homepage][:update][:page_title],
                                 :page_content => @node[:app_homepage][:update][:page_content] }
