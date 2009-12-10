457bb9a1956260ce629b95e32988a8f3ab5245ba
Raphael Simon <raphael@rightscale.com>
2009-12-10 11:16:27 -0800

    Remove obsolete fix

--- cookbooks/app_homepage/recipes/update.rb
+++ cookbooks/app_homepage/recipes/update.rb
@@ -4,8 +4,3 @@ template '/mnt/www/index.html' do
              :page_title   => @node[:app_homepage][:page_title] )
 end
 
-ruby 'fix ports.conf' do
-  ports_path = '/etc/apache2/ports.conf'
-  content = IO.read(ports_path).gsub('8000', '80')
-  File.open('ports_path', 'w') { |f| f.puts content }
-end
