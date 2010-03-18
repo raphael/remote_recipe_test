5c92adad7eb9f62ae2daa4ef12e09b515106fcc1
Raphael Simon <raphael@rightscale.com>
2010-03-18 11:34:49 -0700

    Added gem test cookbook

---
+++ cookbooks/gem_test/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "RightScale, Inc."
+maintainer_email "support@rightscale.com"
+license          IO.read(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'LICENSE')))
+description      "Install Rails."
+version          "0.0.1"
+
+recipe  "gem_test", "Install rails."
+
